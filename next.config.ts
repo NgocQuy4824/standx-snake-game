e328291b873894f5c01e96fda4b9f08c8cd2e67a
NgocQuy <ngquy04082004@gmail.com>
2026-09-01 19:00:49 +0700

    fix: remove standalone output for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Removes the X-Powered-By: Next.js response header
   poweredByHeader: false,
-  output: "standalone",
 };
 
 export default nextConfig;
